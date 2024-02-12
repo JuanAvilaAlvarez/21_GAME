064c90adc45c4a41e1afdd063974eefeb9cd4f20
JuanAvilaAlvarez <avila.avarez@gmail.com>
2024-02-12 07:44:52 -0500

    Optimizacion

--- juego.py
+++ juego.py
@@ -5,7 +5,12 @@ figuras = ["pica", "diamante", "trebol", "corazon"]
 numeros = ["A", "J", "Q", "K"] + [str(i) for i in range (2,11)]
 mazo = [(a,b) for a in figuras for b in numeros]
 
+#Generacion de variables
 c = 0
+casa = []
+contador_casa = 0
+juego = []
+contador = 0
 
 def nueva_carta(lugar):
     global c
@@ -20,29 +25,30 @@ def nueva_carta(lugar):
     c += 1
     return numero
 
-def sumar(numero):
+def sumar(numero, valor):
     if numero in ["J", "Q", "K"]:
         return 10
     elif numero == "A":
-        return 1
+        if valor <= 10:
+            return 11
+        else:
+            return 1
     else:
         return numero
 
 #Casa
-casa = []
-contador_casa = 0
+
 while c < 2:
     nueva_carta("casa")
-    contador_casa += int(sumar(numero))
+    contador_casa += int(sumar(numero, contador_casa))
 
 print(f"El mazo de la casa tiene un {casa[0]}")
 
 #Juego
-juego = []
-contador = 0
+
 while c < 4:
     nueva_carta("juego")
-    contador += int(sumar(numero))
+    contador += int(sumar(numero, contador))
 
 print(f"Tu juego es: {juego}")
 continuar = input("¿Deseas continuar?(si/no): ")
@@ -50,7 +56,7 @@ continuar = input("¿Deseas continuar?(si/no): ")
 if continuar == "si":
     while True:
         nueva_carta("juego")
-        contador += int(sumar(numero))
+        contador += int(sumar(numero, contador))
         if contador > 21:
             print(f"Te has pasado, tu puntaje actual es de: {contador}")
             break
@@ -64,7 +70,7 @@ if continuar != "si":
     while True:
         if contador_casa < contador:
             nueva_carta("casa")
-            contador_casa += int(sumar(numero))
+            contador_casa += int(sumar(numero, contador_casa))
         elif contador_casa > 21:
             print(f"Le has ganado a la casa, esta tiene un puntaje de {contador_casa} y tu tienes un puntaje de {contador}")
             break
